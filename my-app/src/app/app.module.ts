dae420a7abf17c3074b2dda63ca98ffc2dad02bc
NickKettler <nickakettler@gmail.com>
2018-11-27 16:33:26 -0500

    Added Brew and Brew-Review Components Removed default html front page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrewComponent } from './brew/brew.component';
+import { BrewReviewComponent } from './brew-review/brew-review.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrewComponent,
+    BrewReviewComponent
   ],
   imports: [
     BrowserModule,
